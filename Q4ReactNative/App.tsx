a7b7a85deb0c83201487b5e6b82e2f7c2d21f632
winterevil <long.lethanh.cit21@eiu.edu.vn>
2025-10-15 10:43:32 +0700

    Q4_code

---
+++ Q4ReactNative/App.tsx
@@ -0,0 +1,58 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+// import { NewAppScreen } from '@react-native/new-app-screen';
+// import { ScrollView, StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+// import {
+//   SafeAreaProvider,
+//   useSafeAreaInsets,
+// } from 'react-native-safe-area-context';
+
+// function App() {
+//   const isDarkMode = useColorScheme() === 'dark';
+
+//   return (
+//     <SafeAreaProvider>
+//       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+//       <AppContent />
+//     </SafeAreaProvider>
+//   );
+// }
+
+// function AppContent() {
+//   const safeAreaInsets = useSafeAreaInsets();
+
+//   return (
+//     <View style={styles.container}>
+//       <NewAppScreen
+//         templateFileName="App.tsx"
+//         safeAreaInsets={safeAreaInsets}
+//       />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+// });
+import React from 'react';
+import { View, Text, StyleSheet, ScrollView, Button, Alert } from 'react-native';
+import data from './Data';
+import Square from './Square';
+import styles from './style';
+
+export default App = () => {
+  return (
+    <ScrollView style={styles.container}>
+      {data.map((item, index) => (
+        <Square key={item} text={`Square ${index + 1} Thành Long`} />
+      ))}
+    </ScrollView>
+  );
+};
